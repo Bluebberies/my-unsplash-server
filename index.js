78c67167cd3539186ce9c222a5bd3776aec76a3e
Francis <francisalexander000@gmail.com>
2022-09-20 14:40:18 +0100

    added cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const winston = require('winston')
 require('dotenv').config()
+const cors = require('cors')
 
+app.use(cors())
 require('./startup/logging')()
-require("./startup/requestAnalysers")(app);
-require("./startup/uploads")(app);
+require('./startup/requestAnalysers')(app)
+require('./startup/uploads')(app)
 require('./startup/prod')(app)
 
 const port = process.env.PORT
